229284e643288fc6315cd2b3517420e1c6a8bf2c
Kalaimani14 <skkalaimani472@gmail.com>
2026-02-01 11:29:08 +0530

    first commit

---
+++ src/Componants/TopNav.js
@@ -0,0 +1,34 @@
+import "../Style/TopNav.css"
+export default function TopNav() {
+
+    return (
+        <>
+            <div className="container top-nav">
+                <div className="max-w">
+                    <div className="grid grid-2">
+                        <div className="grid-sec grid">
+                            <div className="group grid">
+                                <svg>email_logo</svg>
+                                <a>abc12@gmail.com</a>
+                            </div>
+                            <div className="group grid">
+                                <svg>PH-logo</svg>
+                                <a>+9132142585</a>
+                            </div>
+                        </div>
+                        <div className="grid-sec grid">
+                           <div className="group grid">
+                                <svg>Whatsapplogo</svg>
+                                <a>Whatsapp_Support</a>
+                            </div>
+                            <div className="group grid">
+                                <svg>Yt logo</svg>
+                                <a>YouTub</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
